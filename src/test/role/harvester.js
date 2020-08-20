3515fdd2540a5a9de4a2de926df93424df3f9ce2
Stuart Dodsworth <stuart.dodsworth@jayco.com.au>
2020-08-21 08:38:30 +1000

    removed the clg from the harvesters

--- src/test/role/harvester.js
+++ src/test/role/harvester.js
@@ -2,13 +2,11 @@ var roleHarvester = {
   /** @param {Creep} creep **/
   run: function (creep) {
     if (creep.store.getFreeCapacity() > 0) {
-      console.log("Harvesting");
       var sources = creep.pos.findClosestByPath(FIND_SOURCES);
       if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
         creep.moveTo(sources, { visualizePathStyle: { stroke: "#ffaa00" } });
       }
     } else {
-      console.log("Storing");
       var targets = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
           return (
