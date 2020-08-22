6953742cea015fc441cf59366f877f44299bce35
Symmtom <stuart.dodsworth@hotmail.com>
2020-08-22 11:02:16 +1000

    Added tower defense code

--- src/test/main.js
+++ src/test/main.js
@@ -6,34 +6,44 @@ var roleRepairer = require("role_repairer");
 var population = require("population");
 
 module.exports.loop = function () {
-  population.run();
+	population.run();
 
-  // var closestSource = Game.spawns["Spawn1"].pos.findClosestByPath(FIND_SOURCES);
-  // let path = closestSource.pos.findPathTo(Game.spawns["Spawn1"], {
-  //   ignoreCreeps: true,
-  // });
+	// var closestSource = Game.spawns["Spawn1"].pos.findClosestByPath(FIND_SOURCES);
+	// let path = closestSource.pos.findPathTo(Game.spawns["Spawn1"], {
+	//   ignoreCreeps: true,
+	// });
 
-  // for (let j = 0; j < path.length; ++j) {
-  //   room.createConstructionSite(path[j].x, path[j].y, STRUCTURE_ROAD);
-  // }
+	// for (let j = 0; j < path.length; ++j) {
+	//   room.createConstructionSite(path[j].x, path[j].y, STRUCTURE_ROAD);
+	// }
 
-  for (var name in Game.creeps) {
-    var creep = Game.creeps[name];
-    switch (creep.memory.role) {
-      case "harvester":
-        roleHarvester.run(creep);
-        break;
-      case "upgrader":
-        roleUpgrader.run(creep);
-        break;
-      case "builder":
-        roleBuilder.run(creep);
-        break;
-      case "repairer":
-        roleBuilder.run(creep);
-        break;
-      default:
-        break;
-    }
-  }
+	var towers = Game.spawns["Spawn1"].room.find(FIND_STRUCTURES, {
+		filter: (s) => s.structureType == STRUCTURE_TOWER,
+	});
+	for (let tower of towers) {
+		var target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+		if (target != undefined) {
+			tower.attack(target);
+		}
+	}
+
+	for (var name in Game.creeps) {
+		var creep = Game.creeps[name];
+		switch (creep.memory.role) {
+			case "harvester":
+				roleHarvester.run(creep);
+				break;
+			case "upgrader":
+				roleUpgrader.run(creep);
+				break;
+			case "builder":
+				roleBuilder.run(creep);
+				break;
+			case "repairer":
+				roleBuilder.run(creep);
+				break;
+			default:
+				break;
+		}
+	}
 };
